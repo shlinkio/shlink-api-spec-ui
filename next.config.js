406f3060a98d33f5a16e8948733671a7623fe045
Alejandro Celaya <alejandro@alejandrocelaya.com>
2019-09-01 22:31:41 +0200

    Created swagger-config

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@ const withCSS = require('@zeit/next-css');
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 
 module.exports = withCSS({
+  experimental: {
+    publicDirectory: true,
+  },
   webpack(config) {
     config.optimization.minimizer = config.optimization.minimizer || [];
     config.optimization.minimizer.push(new OptimizeCSSAssetsPlugin({}));
