5f5dbe200a9999b9a248247ad7756da5001a9830
Alejandro Celaya <alejandrocelaya@gmail.com>
2025-05-10 20:19:49 +0200

    Remove react-headroom

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,5 @@
 import type { ChangeEvent, FC } from 'react';
 import { useEffect, useState } from 'react';
-import Headroom from 'react-headroom';
 import { useResolveVersion, useRouter } from '../helpers';
 import { ShlinkLogo } from './ShlinkLogo';
 
@@ -22,23 +21,21 @@ export const Header: FC<HeaderProps> = ({ setTag, tags }) => {
   }, [resolvedVersion]);
 
   return (
-    <Headroom style={{ zIndex: 99 }}>
-      <header className="bg-shlink-brand h-16 py-4">
-        <div className="container mx-auto px-5 flex">
-          <h2 className="text-white text-2xl font-bold flex-1">
-            <ShlinkLogo className="w-8 h-8 inline" />{' '}
-            Shlink<span className="hidden md:inline"> - <small>The URL shortener</small></span>
-          </h2>
-          <select
-            className="w-48 p-1 rounded-sm drop-shadow-md border border-black bg-gray-100"
-            value={activeTag}
-            onChange={onTagChange}
-          >
-            {!tags.length && <option>Loading...</option>}
-            {tags.map((tag: string) => <option key={tag} value={tag}>Shlink {tag}</option>)}
-          </select>
-        </div>
-      </header>
-    </Headroom>
+    <header className="bg-shlink-brand h-16 py-4 fixed top-0 w-full z-30">
+      <div className="container mx-auto px-5 flex">
+        <h2 className="text-white text-2xl font-bold flex-1">
+          <ShlinkLogo className="w-8 h-8 inline" />{' '}
+          Shlink<span className="hidden md:inline"> - <small>The URL shortener</small></span>
+        </h2>
+        <select
+          className="w-48 p-1 rounded-sm drop-shadow-md border border-black bg-gray-100"
+          value={activeTag}
+          onChange={onTagChange}
+        >
+          {!tags.length && <option>Loading...</option>}
+          {tags.map((tag: string) => <option key={tag} value={tag}>Shlink {tag}</option>)}
+        </select>
+      </div>
+    </header>
   );
 };
